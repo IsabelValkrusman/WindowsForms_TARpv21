8bd6163925ff4f7952d27fe0308739044b5051cc
opilane <opilane@HP-CZC6297B89>
2022-09-20 14:57:15 +0300

    WindowsForms

--- WindowsForms_TARpv21/MinuVorm.cs
+++ WindowsForms_TARpv21/MinuVorm.cs
@@ -8,13 +8,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace WindowsForms
 {
-    public partial class MinuVorm : Form
+    public partial class MinuVorm :Form
     {
         TreeView puu;
         Button nupp;
         Label silt;
+        CheckBox mruut1, mruut2;
+        RadioButton rnupp1, rnupp2, rnupp3, rnupp4;
+        PictureBox pilt;
+        ProgressBar riba, aeg;
+
         public MinuVorm()
         {
             //InitializeComponent();
@@ -29,6 +36,9 @@ namespace WindowsForms
             oksad.Nodes.Add(new TreeNode("Nupp-Button"));
             oksad.Nodes.Add(new TreeNode("Silt-Label"));
             oksad.Nodes.Add(new TreeNode("DialogAken-MessageBox"));
+            oksad.Nodes.Add(new TreeNode("Märkeruut-Checkbox"));
+            oksad.Nodes.Add(new TreeNode("Radionupp-RadioButton"));
+            oksad.Nodes.Add(new TreeNode("Edenemisriba-ProgressBar"));
 
             puu.AfterSelect += Puu_AfterSelect;
             puu.Nodes.Add(oksad);
@@ -37,6 +47,31 @@ namespace WindowsForms
 
         private void Puu_AfterSelect(object sender, TreeViewEventArgs e)
         {
+            silt = new Label // внизу разные свойства для элементов
+            {
+                Text = "Minu esimene vorm",
+                Size = new Size(200, 50),
+                Location = new Point(200, 80),
+                Width = 100,
+                Height = 20,
+                ForeColor = Color.White,
+                BackColor = Color.Blue,
+                BorderStyle = BorderStyle.FixedSingle,
+            };
+            mruut1 = new CheckBox
+            {
+                Checked = false,
+                Text = "Üks",
+                Location = new Point(silt.Left + silt.Width, 0),
+                Height = 25,
+            };
+            mruut2 = new CheckBox
+            {
+                Checked = false,
+                Text = "Kaks",
+                Location = new Point(silt.Left + silt.Width, mruut1.Height),
+                Height = 25,
+            };
             if (e.Node.Text == "Nupp-Button")
             {
                 nupp = new Button();
@@ -74,10 +109,160 @@ namespace WindowsForms
                 {
                     this.Close();
                 }
-                else if (vastus == DialogResult.No)
+                else
                 {
-                    MessageBox.Show("Siis töötame edasi", "Vastus oli NO");
+                    MessageBox.Show("Sis töötame edasi lapsed");
                 }
+            }
+            else if (e.Node.Text == "Märkeruut-Checkbox")
+            {
+                mruut1 = new CheckBox
+                {
+                    Checked = false,
+                    Text = "Üks",
+                    Location = new Point(silt.Left + silt.Width, 0),
+                    Height = 25,
+                };
+                mruut2 = new CheckBox
+                {
+                    Checked = false,
+                    Text = "Kaks",
+                    Location = new Point(silt.Left + silt.Width, mruut1.Height),
+                    Height = 25,
+                };
+                mruut1.CheckedChanged += Mruut1_CheckedChanged;
+                mruut2.CheckedChanged += Mruut2_CheckedChanged;
+
+
+                this.Controls.Add(mruut1);
+                this.Controls.Add(mruut2);
+            }
+            else if (e.Node.Text == "Radionupp-RadioButton")
+            {
+                rnupp1 = new RadioButton
+                {
+                    Text = "Paremale",
+                    Width = 112,
+                    Location = new Point(mruut2.Left + mruut2.Width, mruut1.Height + mruut2.Height),
+
+                };
+                rnupp2 = new RadioButton
+                {
+                    Text = "Vasakule",
+                    Width = 112,
+                    Location = new Point(mruut2.Left + mruut2.Width + rnupp1.Width, mruut1.Height + mruut2.Height),
+
+                };
+                rnupp3 = new RadioButton
+                {
+                    Text = "Ülesse",
+                    Width = 112,
+                    Location = new Point(mruut2.Left + mruut2.Width + rnupp1.Width + rnupp2.Width, mruut1.Height + mruut2.Height),
+
+                };
+                rnupp4 = new RadioButton
+                {
+                    Text = "Alla",
+                    Width = 112,
+                    Location = new Point(mruut2.Left + mruut2.Width + rnupp1.Width + rnupp2.Width + rnupp3.Width, mruut1.Height + mruut2.Height),
+
+                };
+                rnupp1.CheckedChanged += new EventHandler(Rnuppud_Changed);
+                rnupp2.CheckedChanged += new EventHandler(Rnuppud_Changed);
+                rnupp3.CheckedChanged += new EventHandler(Rnuppud_Changed);
+                rnupp4.CheckedChanged += new EventHandler(Rnuppud_Changed);
+
+                pilt = new PictureBox
+                {
+                    Image = new Bitmap("france.png"),
+                    Location = new Point(300, 450),
+                    Size = new Size(120, 120),
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                };
+
+                this.Controls.Add(rnupp1);
+                this.Controls.Add(rnupp2);
+                this.Controls.Add(rnupp3);
+                this.Controls.Add(rnupp4);
+                this.Controls.Add(pilt);
+            }
+            else if(e.Node.Text=="Edenemisriba-Progressibar")
+            {
+                riba = new ProgressBar
+                {
+                    Width = 400,
+                    Height = 30,
+                    Location = new Point(350,500),
+                    Value=50,
+                    Minimum=0,
+                    Maximum=100,
+                    Step=1,
+                   // Dock=DockStyle.Bottom,
+                };
+                aeg = new Timer();
+                aeg.Enabled = true;
+                aeg.Tick += Aeg_Tick;
+                this.Controls.Add(riba);
+            }
+        }
+        private void Aeg_Tick(object sender, EventArgs e)
+        {
+            riba.PerformStep();
+        }
+
+        private void Rnuppud_Changed(object sender, EventArgs e)
+        {
+            if(rnupp1.Checked)
+            {
+                pilt.Location=new Point(pilt.Left+10, pilt.Top);
+                rnupp1.Checked = false;
+            }
+            if(rnupp2.Checked)
+            {
+                pilt.Location = new Point(pilt.Left - 10, pilt.Top);
+                rnupp1.Checked = false;
+            }
+            if (rnupp3.Checked) pilt.Location = new Point(pilt.Left, pilt.Top - 10);
+            if (rnupp3.Checked) pilt.Location = new Point(pilt.Left, pilt.Top + 10);
+        }
+
+        private void Mruut2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (mruut1.Checked == true && mruut2.Checked == true)
+            {
+                var vastus = MessageBox.Show("esimene katse");
+            }
+            else if (mruut1.Checked == false && mruut2.Checked == false)
+            {
+                var vastus = MessageBox.Show("teine katse");
+            }
+            else if (mruut1.Checked == true && mruut2.Checked == false)
+            {
+                var vastus = MessageBox.Show("kolmas katse");
+            }
+            else if (mruut1.Checked == false && mruut2.Checked == true)
+            {
+                var vastus = MessageBox.Show("neljas katse");
+            }
+        }
+
+        private void Mruut1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (mruut1.Checked == true && mruut2.Checked == true)
+            {
+                var vastus = MessageBox.Show("esimene katse");
+            }
+            else if (mruut1.Checked == false && mruut2.Checked == false)
+            {
+                var vastus = MessageBox.Show("teine katse");
+            }
+            else if (mruut1.Checked == true && mruut2.Checked == false)
+            {
+                var vastus = MessageBox.Show("kolmas katse");
+            }
+            else if (mruut1.Checked == false && mruut2.Checked == true)
+            {
+                var vastus = MessageBox.Show("neljas katse");
             }
         }
 
