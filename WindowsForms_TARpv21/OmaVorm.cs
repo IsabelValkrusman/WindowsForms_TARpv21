1ddc6f2ae6b510078dc3a8eda77486619b08058f
IsabelValkrusman <77299939+IsabelValkrusman@users.noreply.github.com>
2022-09-27 12:10:35 +0300

    Update OmaVorm.cs

--- WindowsForms_TARpv21/OmaVorm.cs
+++ WindowsForms_TARpv21/OmaVorm.cs
@@ -11,25 +11,28 @@ using System.Windows.Forms;
 
 namespace WindowsForms_TARpv21
 {
-    public class OmaVorm:Form
+   public class OmaVorm : Form
     {
         public OmaVorm() { }
-        RadioButton nupp1, nupp2, nupp3;
+        RadioButton nupp1, nupp2;
+        //private string v1;
+        //private string v2;
+        //private string v3;
 
         public OmaVorm(string Peakiri, string Nupp, string Fail)
         {
             this.ClientSize = new System.Drawing.Size(300, 300);
-            this.Text= Peakiri;
+            this.Text = Peakiri;
             Button nupp = new Button
             {
                 Text = Nupp,
-                Location=new System.Drawing.Point(50,50),
-                Size=new System.Drawing.Size(100,50),
+                Location = new System.Drawing.Point(50, 50),
+                Size = new System.Drawing.Size(100, 50),
                 BackColor = System.Drawing.Color.AliceBlue,
 
             };
             nupp.Click += Nupp_Click;
-            
+
             Label failinimi = new Label
             {
                 Text = Fail,
@@ -37,10 +40,35 @@ namespace WindowsForms_TARpv21
                 Size = new System.Drawing.Size(100, 50),
                 BackColor = System.Drawing.Color.AliceBlue,
             };
+
+            nupp1 = new RadioButton
+            {
+                Text = "nupp",
+                Location = new System.Drawing.Point(50, 150),
+                Size = new System.Drawing.Size(100, 25),
+                BackColor = System.Drawing.Color.AliceBlue,
+            };
+
+
+            nupp2 = new RadioButton
+            {
+                Text = "nupp",
+                Location = new System.Drawing.Point(50, 175),
+                Size = new System.Drawing.Size(100, 20),
+                BackColor = System.Drawing.Color.AliceBlue,
+            };
             this.Controls.Add(nupp);
             this.Controls.Add(failinimi);
+            this.Controls.Add(nupp2);
+            this.Controls.Add(nupp1);
         }
 
+        /*public OmaVorm(string v1, string v2, string v3)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+        }*/
 
         private void Nupp_Click(object sender, EventArgs e)
         {
@@ -49,43 +77,44 @@ namespace WindowsForms_TARpv21
             if (vastus == DialogResult.Yes)
             {
                 if (nupp1.Checked == true)
+
                 {
                     using (var muusika = new SoundPlayer(@"..\..\music.wav"))
+                    {
+                        MessageBox.Show("Muusika mängib");
+                        muusika.Play();
+
+
+                    }
+
+
+                    nupp1.Checked = false;
+                }
+
+                else if (nupp2.Checked == true)
                 {
-                    MessageBox.Show("Muusika mängib");
-                    muusika.Play();
+                    using (var muusika = new SoundPlayer(@"..\..\sound.wav"))
+                    {
+                        muusika.Play();
+                        MessageBox.Show("mängib: ", "Muusika");
+
+                    }
+                    nupp2.Checked = false;
+                }
+
+                else
+                {
+                    MessageBox.Show("Mitte midagi ei ole valitud");
+                }
+            }
+            else
+            {
+
+                MessageBox.Show(":(");
 
                 }
                     
 
-                        nupp1.Checked = false;
-                }
-
-                else if (nupp2.Checked==true)
-            {
-                    using (var muusika = new SoundPlayer(@"..\..\musika.wav"))
-                    {
-                        muusika.Play();
-                        MessageBox.Show("mängib: ","Muusika");
-                        
-                    }
-                        nupp2.Checked=false;
-            }
-
-            else
-            {
-                    MessageBox.Show("Mitte midagi ei valitud!");
-            }          
-            else
-            {
-                  MessageBox.Show("Mitte midagi ei ole valitud");
-            }
-            }
-            else
-            {
-    
-                    MessageBox.Show(":(");
-            }
         }
 
     }
